dcff291a7794c7c21c12221822572ee41fffe64d
Nihkil Tyagi <tyagin@jcsoftwaresolution.com>
2025-11-13 14:45:02 +0530

    design changes

--- src/Pages/form-1/list.jsx
+++ src/Pages/form-1/list.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../Components/Layout";
+import { IoTrashOutline } from "react-icons/io5";
+import { FaRegEdit } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const List = () => {
+  const navigate = useNavigate();
   const [form, setForm] = useState({
     fName: "",
     lName: "",
@@ -20,62 +24,68 @@ const List = () => {
       setData(getData);
     }
   }, [form]);
+  const handleDelete = (id) => {
+    const deletedValue = data.filter((val) => val.id !== id) || [];
+    setData(deletedValue);
+    localStorage.setItem("form", JSON.stringify(deletedValue));
+  };
   return (
     <div>
       <Layout>
         <div className=" bg-[#fff] min-h-screen pt-10">
-        <div className="py-20 px-5 container mx-auto">
-          <div className="w-full overflow-auto ">
-            <table className="w-full ">
-              <thead>
-                <tr>
-                  <th className="px-3 py-2 text-left border-b ">First Name</th>
-                  <th className="px-3 py-2 text-left border-b ">Last Name</th>
-                  <th className="px-3 py-2 text-left border-b ">Email</th>
-                  <th className="px-3 py-2 text-left border-b ">Mobile No.</th>
-                  <th className="px-3 py-2 text-left border-b ">City</th>
-                  <th className="px-3 py-2 text-left border-b ">State</th>
-                  <th className="px-3 py-2 text-left border-b ">Hobies</th>
-                  <th className="px-3 py-2 text-left border-b ">Skills</th>
-                  <th className="px-3 py-2 text-left border-b ">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((info, i) => (
+          <div className="py-20 px-5 container mx-auto">
+            <div className="w-full overflow-auto border rounded  ">
+              <table className="w-full">
+                <thead>
                   <tr>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.fName}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.lName}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.email}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.mobileNo}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.city}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.state}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.hobbies}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      {info?.skills}
-                    </td>
-                    <td className="px-3 py-2 text-left border-b ">
-                      <div className="flex gap-2 "></div>
-                    </td>
+                    <th className="px-3 py-2 text-left border-b ">
+                      First Name
+                    </th>
+                    <th className="px-3 py-2 text-left border-b ">Last Name</th>
+                    <th className="px-3 py-2 text-left border-b ">Email</th>
+                    <th className="px-3 py-2 text-left border-b ">
+                      Mobile No.
+                    </th>
+                    <th className="px-3 py-2 text-left border-b ">City</th>
+                    <th className="px-3 py-2 text-left border-b ">State</th>
+                    <th className="px-3 py-2 text-left border-b ">Hobies</th>
+                    <th className="px-3 py-2 text-left border-b ">Skills</th>
+                    <th className="px-3 py-2 text-left border-b ">Actions</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {data.map((info, i) => (
+                    <tr className=" border-b last:border-none">
+                      <td className="px-3 py-2 text-left ">{info?.fName}</td>
+                      <td className="px-3 py-2 text-left ">{info?.lName}</td>
+                      <td className="px-3 py-2 text-left ">{info?.email}</td>
+                      <td className="px-3 py-2 text-left ">{info?.mobileNo}</td>
+                      <td className="px-3 py-2 text-left ">{info?.city}</td>
+                      <td className="px-3 py-2 text-left ">{info?.state}</td>
+                      <td className="px-3 py-2 text-left ">{info?.hobbies}</td>
+                      <td className="px-3 py-2 text-left ">{info?.skills}</td>
+                      <td className="px-3 py-2 text-left ">
+                        <div className="flex gap-2 ">
+                          <button
+                            onClick={() => navigate("/add")}
+                            className="bg-green-500 p-2 rounded-md cursor-pointer hover:opacity-80 "
+                          >
+                            <FaRegEdit className="text-[#fff] text-[18px] " />
+                          </button>
+                          <button
+                            onClick={() => handleDelete(info.id)}
+                            className="bg-red-500 p-2 rounded-md cursor-pointer hover:opacity-80 "
+                          >
+                            <IoTrashOutline className="text-[#fff] text-[18px] " />
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
         </div>
       </Layout>
     </div>
